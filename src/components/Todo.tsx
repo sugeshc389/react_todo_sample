a11e91661fb7d5eb6b4768682f2725d051fec76a
sugesh chandra m k <sugeshc389@gmail.com>
2024-01-08 19:23:25 +0530

    updated

---
+++ src/components/Todo.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+import "../css/Todo.css"
+
+type TodoItem = {
+    No: number;
+    Text: string;
+    Display: string;
+  };
+
+function Todo() {
+    const [todo, setTodo] = useState<TodoItem[]>([]);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    let count = 0;
+    const add = () => {
+        setTodo([...todo, { No: count+count, Text: inputRef.current.value, Display: '' }])
+        // inputRef.current.value = ""; 
+    }
+
+    useEffect(()=>{
+console.log(todo);
+
+    },[todo])
+    return (
+        <div className='todo'>
+            <div className="todo-header">  To-Do List     </div>
+            <div className="todo-add">
+                <input ref={inputRef} type="text" placeholder="Add Your Task...." className="todo-input" />
+                <div onClick={()=>{add()}} className="todo-add-btn">Add</div>
+            </div>
+            <div className="todo-list">
+                {todo.map((item,index)=>{
+                    return <h1 key={index}>{item.No}:{item.Text}</h1>
+                })}
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Todo
